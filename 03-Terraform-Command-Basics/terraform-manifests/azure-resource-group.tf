aac60813153624d420177cf183b0cd34a0cca49f
Phil Waller <philwaller5269@gmail.com>
2021-08-23 21:14:52 +0100

    updatd all

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
+resource "azurerm_resource_group" "pwaller_rg1" {
   location = "ukwest"
-  name = "my-demo-rg1"  
+  name = "pwaller-rg1"  
 }
