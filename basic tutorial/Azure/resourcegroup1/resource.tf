f328516d98550472b676995f5e7fa8dbb240c3e7
Phil Waller <azuread5269@gmail.com>
2021-08-27 22:09:55 +0100

    updated all

---
+++ basic tutorial/Azure/resourcegroup1/resource.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.73.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  # subscription_id = "4cc7495e-1417-40c7-8d36-993c0d7027f5"
+
+
+}
+
+resource "azurerm_resource_group" "pwaller_rg112" {
+  name     = "pwaller_rg112"
+  location = "West Europe"
+}
