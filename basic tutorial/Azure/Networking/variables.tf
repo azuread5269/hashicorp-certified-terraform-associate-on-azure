8c5670f7ba2c898dd1083db441d912f7be7597cc
Phil Waller <azuread5269@gmail.com>
2021-09-10 21:59:58 +0100

    all

--- basic tutorial/Azure/Networking/variables.tf
+++ basic tutorial/Azure/Networking/variables.tf
@@ -1,13 +1,13 @@
-# variable "resource_group_name" {
-#     type        = list
-#     default     = ["pw_rg1","pw_rg2"]
-#     description = "RG name in Azure"
-# }
+variable "resource_group_name" {
+    type        = string
+    # default     = ["pw_rg1","pw_rg2"]
+    description = "RG name in Azure"
+}
 
-# variable "resource_group_location" {
-#     type        = string
-#     description = "RG location in Azure"
-# }
+variable "resource_group_location" {
+    type        = string
+    description = "RG location in Azure"
+}
 
 variable "storage_account_name" {
     type        = string
