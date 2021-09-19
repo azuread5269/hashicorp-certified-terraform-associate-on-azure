0eca0be1403a27717f5bd6afc811d6ddba95b16d
Phil Waller <azuread5269@gmail.com>
2021-09-19 16:12:20 +0100

    updated all

--- basic tutorial/Azure/loadbalancing/main.tf
+++ basic tutorial/Azure/loadbalancing/main.tf
@@ -10,27 +10,27 @@ terraform {
       version = ">=3.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name = "pw5269resourcegroup"
-    storage_account_name = "pw5269storage"
-    container_name = "pw5269storagecontainer"
-    key = "terraform.state"
-  }
+  # backend "azurerm" {
+  #   resource_group_name = "pw5269resourcegroup"
+  #   storage_account_name = "pw5269storage"
+  #   container_name = "pw5269storagecontainer"
+  #   key = "terraform1.state"
+  # }
 }
 
 provider "azurerm" {
   # Configuration options
   features {}
-  # subscription_id = "b312c3d5-5b7c-4ab6-ae52-2b3a94b9c232" #= subscription_id
-  # client_id       = "a9d85e3e-105d-4375-894e-1086362f02c9" # = appid from above 
-  # client_secret   = "w2E7Q~1NUKFekO88RKHR64v_uo7Oi46NJdyiK" #= password from above 
-  # tenant_id       = "d4cfe037-3ef4-42d7-a3f8-74f292af0782" #= tenant from above 
+  subscription_id = "b312c3d5-5b7c-4ab6-ae52-2b3a94b9c232" #= subscription_id
+  client_id       = "a9d85e3e-105d-4375-894e-1086362f02c9" # = appid from above 
+  client_secret   = "w2E7Q~1NUKFekO88RKHR64v_uo7Oi46NJdyiK" #= password from above 
+  tenant_id       = "d4cfe037-3ef4-42d7-a3f8-74f292af0782" #= tenant from above 
 }
 
-# resource "azurerm_resource_group" "pwrg1" {
-#   name = var.resource_group_name
-#   location = var.resource_group_location 
-# }
+resource "azurerm_resource_group" "pwrg1" {
+  name = var.resource_group_name
+  location = var.resource_group_location 
+}
 
 resource "azurerm_virtual_network" "pw5269vn1" {
   name                = var.azurerm_virtual_network
@@ -98,6 +98,18 @@ resource "azurerm_public_ip" "pw5269pubip1" {
     }
 }
 
+resource "azurerm_public_ip" "pw5269pubip2" {
+    name                         = var.public_ip_address
+    location                     = var.resource_group_location
+    resource_group_name          = var.resource_group_name
+    allocation_method            = "Dynamic"
+    domain_name_label            = "pw5269vm2"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
 resource "azurerm_network_interface" "pw5269nic1" {
   name                = var.network_interface_name
   location            = var.resource_group_location
@@ -120,13 +132,49 @@ resource "azurerm_network_interface" "pw5269nic1" {
 
 }
 
-# Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "pw5269nictonsg" {
-    network_interface_id      = azurerm_network_interface.pw5269nic1.id
-    network_security_group_id = azurerm_network_security_group.pw5269nsg1.id
+resource "azurerm_network_interface" "pw5269nic2" {
+  name                = var.network_interface_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "lan"
+    subnet_id                     = azurerm_subnet.pw5269subs[0].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pw5269pubip2.id
+  }
+
+  depends_on = [
+    azurerm_subnet.pw5269subs,
+  ]
+
+      tags = {
+        environment = "Terraform Demo"
+    }
+
 }
 
-## need to apply nsg to subnet !!!!
+# # Connect the security group to the network interface
+# resource "azurerm_network_interface_security_group_association" "pw5269nictonsg1" {
+#     network_interface_id      = azurerm_network_interface.pw5269nic1.id
+#     network_security_group_id = azurerm_network_security_group.pw5269nsg1.id
+# }
+
+# ## need to apply nsg to subnet !!!!
+
+# # Connect the security group to the network interface
+# resource "azurerm_network_interface_security_group_association" "pw5269nictonsg2" {
+#     network_interface_id      = azurerm_network_interface.pw5269nic2.id
+#     network_security_group_id = azurerm_network_security_group.pw5269nsg2.id
+# }
+
+resource "azurerm_subnet_network_security_group_association" "pw5269nictonsgs" {
+  subnet_id =   azurerm_subnet.pw5269subs[0].id
+  network_security_group_id = azurerm_network_security_group.pw5269nsg1.id
+}
+
+
+
 
 resource "azurerm_virtual_machine" "pw5269vm1" {
   name                  = "${var.vm_name_prefix}-vm"
@@ -151,7 +199,7 @@ storage_os_disk {
   
   }
 os_profile {
-  computer_name  = "pw52692019DC"
+  computer_name  = "pw52692019DC1"
   admin_username = "pwaller"
   admin_password = "Pa55w0rd101!"
   }
@@ -165,8 +213,45 @@ os_profile {
     
   }
 
-  # boot_diagnostics {
-  #   enabled = "false"
-  # }
+
+}
+
+
+resource "azurerm_virtual_machine" "pw5269vm2" {
+  name                  = "${var.vm_name_prefix}-vm"
+  location              = var.resource_group_location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.pw5269nic2.id]
+  vm_size               = "Standard_B1s"
+  delete_data_disks_on_termination = true
+
+storage_image_reference {
+  publisher = "microsoftwindowsserver"
+  offer     = "windowsserver"
+  sku       = "2019-Datacenter"
+  version   = "latest"
+  }
+
+storage_os_disk {
+  name              = "pw5269disk2"
+  caching           = "ReadWrite"
+  create_option     = "FromImage"
+  managed_disk_type = "Standard_LRS"
+  
+  }
+os_profile {
+  computer_name  = "pw52692019DC2"
+  admin_username = "pwaller"
+  admin_password = "Pa55w0rd101!"
+  }
+
+  os_profile_windows_config {
+  provision_vm_agent = true
+  
+     winrm {
+         protocol = "http"
+        }
+    
+  }
 
 }
