7805918fd4b265f151517b2d61b47a0ae4ee2cf0
Phil Waller <azuread5269@gmail.com>
2021-09-22 21:57:49 +0100

    updated all

--- basic tutorial/Azure/customscriptaction/ansible-enable.ps1
+++ basic tutorial/Azure/customscriptaction/ansible-enable.ps1
@@ -2,54 +2,63 @@
 param (
     [string]$Password
 )
+write-verbose "importing AZ Powershell Module into current session" -verbose
+Import-Module az
+$Mod = Get-Module az
+IF (($Mod))
+{
+    Write-Verbose "Create credentials for Azure portal" -Verbose
+    $User = "azauto@pwazure5269.co.uk"
+    # $Pass = "Camberwe!!Carr0t!" | ConvertTo-SecureString -AsPlainText -Force
+    $Pass = $Password | ConvertTo-SecureString -AsPlainText -Force
+    $Creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$Pass
 
-Write-Verbose "Create credentials for Azure portal" -Verbose
-$User = "azauto@pwazure5269.co.uk"
-# $Pass = "Camberwe!!Carr0t!" | ConvertTo-SecureString -AsPlainText -Force
-$Pass = $Password | ConvertTo-SecureString -AsPlainText -Force
-$Creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$Pass
+    Write-Verbose "Log into Azure Portal" -Verbose
+    Connect-AzAccount -Credential $Creds -Subscription b312c3d5-5b7c-4ab6-ae52-2b3a94b9c232  -Tenant d4cfe037-3ef4-42d7-a3f8-74f292af0782
 
-Write-Verbose "Log into Azure Portal" -Verbose
-Connect-AzAccount -Credential $Creds -Subscription b312c3d5-5b7c-4ab6-ae52-2b3a94b9c232  -Tenant d4cfe037-3ef4-42d7-a3f8-74f292af0782
+    Write-Verbose "Obtain VMs in portal" -Verbose
+    $VMName = Get-AzVM | ? {$_.Name -match 'AZ-VM'}
 
-Write-Verbose "Obtain VMs in portal" -Verbose
-$VMName = Get-AzVM | ? {$_.Name -match 'AZ-VM'}
+    Write-Verbose "Obtain Resource Groups in portal" -Verbose
+    $RGname = Get-AzResourceGroup | ? {$_.ResourceGroupName -match 'pw52'}
 
-Write-Verbose "Obtain Resource Groups in portal" -Verbose
-$RGname = Get-AzResourceGroup | ? {$_.ResourceGroupName -match 'pw52'}
+    Write-Verbose "Obtain locations in portal" -Verbose
+    $Location = "ukwest"
 
-Write-Verbose "Obtain locations in portal" -Verbose
-$Location = "ukwest"
+    Write-Verbose "Obtain binary blob contaning ansible script in portal" -Verbose
+    $ScriptblobAccount = "pw5269scriptstore"
+    $Scriptbloburl =  "https://pw5269scriptstore.blob.core.windows.net/pw5269containerscriptstore/"
+    $Scriptblobkey = "6Pt+M1SgiU7c4UHnbEZ24Spm44XozLSCrS+u2Lc6gd3nOpvG+9mOOyClqz40BpL/DsmegLWY55HMcs3mkP8AgA=="
 
-Write-Verbose "Obtain binary blob contaning ansible script in portal" -Verbose
-$ScriptblobAccount = "pw5269scriptstore"
-$Scriptbloburl =  "https://pw5269scriptstore.blob.core.windows.net/pw5269containerscriptstore/"
-$Scriptblobkey = "6Pt+M1SgiU7c4UHnbEZ24Spm44XozLSCrS+u2Lc6gd3nOpvG+9mOOyClqz40BpL/DsmegLWY55HMcs3mkP8AgA=="
+    Write-Verbose "Obtain custom script specifications in portal" -Verbose
+    $Scriptname = "ansiblewinrm.ps1"
+    $Extensionname = "AutoScript"
+    $Extensiontype = "CustomScriptExtension"
+    $publisher = "Microsoft.Compute"
+    $Version = "1.8"
+    $timestamp = (Get-Date).Ticks
 
-Write-Verbose "Obtain custom script specifications in portal" -Verbose
-$Scriptname = "ansiblewinrm.ps1"
-$Extensionname = "AutoScript"
-$Extensiontype = "CustomScriptExtension"
-$publisher = "Microsoft.Compute"
-$Version = "1.8"
-$timestamp = (Get-Date).Ticks
+    Write-Verbose "concatenate script and URL" -Verbose
+    $Scriptlocation = $Scriptbloburl + $Scriptname
+    $Scriptexe = ".\$Scriptname"
 
-Write-Verbose "concatenate script and URL" -Verbose
-$Scriptlocation = $Scriptbloburl + $Scriptname
-$Scriptexe = ".\$Scriptname"
+    Write-Verbose "concatenate URLs and storage accont " -Verbose
+    $PrivateConfiguration = @{"storageAccountName" = "$ScriptBlobAccount";"storageAccountKey" = "$ScriptBlobKey"} 
+    $PublicConfiguration = @{"fileUris" = [Object[]]"$ScriptLocation";"timestamp" = "$timestamp";"commandToExecute" = "powershell.exe -ExecutionPolicy Unrestricted -Command $ScriptExe"}
 
-Write-Verbose "concatenate URLs and storage accont " -Verbose
-$PrivateConfiguration = @{"storageAccountName" = "$ScriptBlobAccount";"storageAccountKey" = "$ScriptBlobKey"} 
-$PublicConfiguration = @{"fileUris" = [Object[]]"$ScriptLocation";"timestamp" = "$timestamp";"commandToExecute" = "powershell.exe -ExecutionPolicy Unrestricted -Command $ScriptExe"}
-
-Write-Verbose "set Extension on VM via Tools" -Verbose
-Set-AzVMExtension -ResourceGroupName $RGName.ResourceGroupName -VMName $VMName.Name -Location $Location `
--Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionType -TypeHandlerVersion $Version `
- -Settings $PublicConfiguration -ProtectedSettings $PrivateConfiguration 
+    Write-Verbose "set Extension on VM via Tools" -Verbose
+    Set-AzVMExtension -ResourceGroupName $RGName.ResourceGroupName -VMName $VMName.Name -Location $Location `
+    -Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionType -TypeHandlerVersion $Version `
+     -Settings $PublicConfiguration -ProtectedSettings $PrivateConfiguration 
 
 
- Write-Verbose "Obtain Extension status" -Verbose
- ((Get-AzVM -Name $VMName.Name -ResourceGroupName $RGName.ResourceGroupName -Status).Extensions | Where-Object {$_.Name -eq $ExtensionName}).Substatuses
+     Write-Verbose "Obtain Extension status" -Verbose
+     ((Get-AzVM -Name $VMName.Name -ResourceGroupName $RGName.ResourceGroupName -Status).Extensions | Where-Object {$_.Name -eq $ExtensionName}).Substatuses
 
- Write-Verbose "Remove extension" -Verbose
- Remove-AzVMExtension -ResourceGroupName $RGName -VMName $VMName -Name $Extensionname -Force
+     Write-Verbose "Remove extension" -Verbose
+     Remove-AzVMExtension -ResourceGroupName $RGName -VMName $VMName -Name $Extensionname -Force
+}
+else
+{
+    exit 1
+}
