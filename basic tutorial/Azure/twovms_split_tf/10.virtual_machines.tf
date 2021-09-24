0c7ffaa4862a812fe1831bf1d9899e2e12edcfa3
Phil Waller <azuread5269@gmail.com>
2021-09-24 21:49:11 +0100

    u

--- basic tutorial/Azure/twovms_split_tf/10.virtual_machines.tf
+++ basic tutorial/Azure/twovms_split_tf/10.virtual_machines.tf
@@ -15,7 +15,7 @@ Write-Verbose "Disable all firewall rules" -Verbose
 Get-NetFirewallProfile | % {Set-NetFirewallProfile -Name $_.name -Enabled False}
 
 new-item c:\myfile.txt 
-add-content c:\myfile.txt ${azurerm_network_interface.rg.name}
+add-content c:\myfile.txt ${azurerm_network_interface.rg[count.index].name}
 
 CUSTOM_DATA
 
