3cf3ed04bdb0ab160ca4bc89cebb4efc5bb25fec
Phil Waller <azuread5269@gmail.com>
2021-09-24 21:25:58 +0100

    updated

--- basic tutorial/Azure/twovms_split_tf/10.virtual_machines.tf
+++ basic tutorial/Azure/twovms_split_tf/10.virtual_machines.tf
@@ -1,4 +1,27 @@
+locals {
+vm_custom_data = <<CUSTOM_DATA
+$GI = Get-Item WSMan:\localhost\Service\AllowUnencrypted
+IF (!($GI.Value -match 'true'))
+{
+    Write-Warning "AllowUnencrypted not already configured, so configuring" -Verbose
+    Set-Item WSMan:\localhost\Service\AllowUnencrypted -value true
+    Set-Item WSMan:\localhost\Service\Auth\Basic -Value true 
+}
+ELSE
+{
+    Write-Verbose "AllowUnencrypted already configured" -Verbose
+}
+Write-Verbose "Disable all firewall rules" -Verbose
+Get-NetFirewallProfile | % {Set-NetFirewallProfile -Name $_.name -Enabled False}
 
+new-item c:\myfile.txt 
+add-content c:\myfile.txt ${azurerm_network_interface.rg.name}
+
+CUSTOM_DATA
+
+}
+ 
+ 
  resource "azurerm_windows_virtual_machine" "rg" {
    count = 2  
    name                = "AZ-VM-00-${count.index}"
@@ -25,4 +48,8 @@
         environment = "Terraform Demo"
     }
 
- }
+    # custom_data = filebase64("${path.module}/assets/configure_winrm_disable_firewall.ps1")
+      
+    }
+
+ 
