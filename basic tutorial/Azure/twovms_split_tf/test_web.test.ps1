02cd4360934de1c0556445caf5a732e94e40ad5a
Phil Waller <azuread5269@gmail.com>
2021-09-25 13:07:34 +0100

    upudated all

---
+++ basic tutorial/Azure/twovms_split_tf/test_web.test.ps1
@@ -0,0 +1,27 @@
+Import-Module Pester 
+
+$getjson = Get-Content terraform.tfstate 
+$Fqdn = $getjson | Select-String '"fqdn":'
+$FqdnToTest = 
+$Fqdn.ForEach({
+[string]$String = $_ 
+($String.split(':')[1]).Replace('"','').Replace(',','').Trim().TrimEnd().TrimStart()
+
+})
+
+
+Describe "test Web-server on Azure" {
+
+    foreach ($thing in $FqdnToTest)
+    {
+        $SC = Invoke-WebRequest $thing
+        it "the status code for $thing should be 200" {
+        $SC.StatusCode | Should be '200'
+
+        }
+
+              
+    }
+
+}
+
