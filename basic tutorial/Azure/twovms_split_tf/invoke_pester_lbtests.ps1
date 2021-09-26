85351948afe635c67afc5c77b7ae514ddbd5e2f5
Phil Waller <azuread5269@gmail.com>
2021-09-26 20:37:35 +0100

    updated all

---
+++ basic tutorial/Azure/twovms_split_tf/invoke_pester_lbtests.ps1
@@ -0,0 +1,8 @@
+    Invoke-Pester C:\terraform\Jobs\twovms_split_tf\testweblb.test.ps1 -OutputFormat NUnitXml -OutputFile  C:\terraform\Jobs\twovms_split_tf\testweblb.test.xml -PassThru
+    [xml]$GetXML = Get-Content C:\terraform\Jobs\twovms_split_tf\testweblb.test.xml
+    $Results = $GetXML.'test-results'.'test-suite'.result
+
+    IF ($Results -match 'Failure')
+    {
+        exit 1
+    }
