de9dff78dbcdf30037cf32fc1dc3fc662a41928f
Phil Waller <azuread5269@gmail.com>
2021-09-27 18:54:34 +0100

    updatd all

--- basic tutorial/Azure/twovms_split_tf/invoke_pester_lbtests.ps1
+++ basic tutorial/Azure/twovms_split_tf/invoke_pester_lbtests.ps1
@@ -1,4 +1,4 @@
-    Invoke-Pester C:\terraform\Jobs\twovms_split_tf\testweblb.test.ps1 -OutputFormat NUnitXml -OutputFile  C:\terraform\Jobs\twovms_split_tf\testweblb.test.xml -PassThru
+    Invoke-Pester C:\terraform\Jobs\twovms_split_tf\test_weblb.test.ps1 -OutputFormat NUnitXml -OutputFile  C:\terraform\Jobs\twovms_split_tf\testweblb.test.xml -PassThru
     [xml]$GetXML = Get-Content C:\terraform\Jobs\twovms_split_tf\testweblb.test.xml
     $Results = $GetXML.'test-results'.'test-suite'.result
 
--- basic tutorial/Azure/twovms_split_tf/invoke_pester_tests.ps1
+++ basic tutorial/Azure/twovms_split_tf/invoke_pester_tests.ps1
@@ -1,4 +1,4 @@
-    Invoke-Pester C:\terraform\Jobs\twovms_split_tf\testweb.test.ps1 -OutputFormat NUnitXml -OutputFile  C:\terraform\Jobs\customscriptaction\testweb.test.xml -PassThru
+    Invoke-Pester C:\terraform\Jobs\twovms_split_tf\test_web.test.ps1 -OutputFormat NUnitXml -OutputFile  C:\terraform\Jobs\customscriptaction\testweb.test.xml -PassThru
     [xml]$GetXML = Get-Content C:\terraform\Jobs\twovms_split_tf\testweb.test.xml
     $Results = $GetXML.'test-results'.'test-suite'.result
 
